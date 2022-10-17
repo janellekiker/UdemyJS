8bed95481a2cd3dff1d1a8d3bfd7cd401bb9c2a0
Janelle Kiker <113371576+janellekiker@users.noreply.github.com>
2022-10-17 11:06:53 -0500

    Add files via upload

---
+++ 03-Developer-Skills/codingChallenge1.js
@@ -0,0 +1,16 @@
+'use strict';
+/*
+// Coding Challenge 1
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    let num = i;
+    let day = num + 1;
+    str = str + `${arr[i]}C in ${day} days ... `;
+  }
+  console.log('... ' + str);
+};
+
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
+*/
