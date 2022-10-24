98a464cd4d331217887a447dd26399e84430e9ed
Janelle Kiker <janellekiker@gmail.com>
2022-10-24 11:12:18 -0500

    can guess numbers now

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,6 +1,58 @@
 "use strict";
-// testing my git skills
-// coding takes practice my dude
 
-let x = 5;
+/*
 console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!🎉";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+console.log(document.querySelector(".guess").value);
+document.querySelector(".guess").value = 23;
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector(".message").textContent = "🙈 No Number! 🙈";
+
+    // When player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number! 🎉";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    // When guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = " ⬇️ Too High! 😔";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        " 💥 You lost the game! 😭 ";
+      document.querySelector("body").style.backgroundColor = "#DC143C";
+      document.querySelector(".score").textContent = 0;
+    }
+
+    // When guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = " ⬆️ Too Low! 😔";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        " 💥 You lost the game! 😭 ";
+      document.querySelector("body").style.backgroundColor = "#DC143C";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
