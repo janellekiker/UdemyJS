faf0f474467954781028bd3e0487189b7c020ec2
Janelle Kiker <janellekiker@gmail.com>
2022-10-25 15:45:21 -0500

    modal DRY coding

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -1,3 +1,23 @@
 "use strict";
 
-//testing github
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsShowModal = document.querySelectorAll(".show-modal");
+
+const showModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener("click", showModal);
+
+  btnCloseModal.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+}
